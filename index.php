33c5dc2f7d8ec64243b774aaaa597a9886608513
jino_exprzn <jino@exprzn.com>
2024-08-23 11:20:03 +0530

    how to show output in php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+echo '<h3>Hwllo</h3>';
+echo 'my last name is mukesh';
+echo '</br>'; 
+print('hello');
+echo '</br>';
+print_r('hello');
+echo '</br>';
+$mynewname = "Sam";
+var_dump($mynewname);
+
+?>
