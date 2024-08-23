29abbe5c0f19647e5406a6a1a82f46819584a6a2
jino_exprzn <jino@exprzn.com>
2024-08-23 11:45:07 +0530

    lesson-1

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+// This is a single comment
+# This is a single comment
+/*
+    this is a multi line comment
+*/
 echo '<h3>Hwllo</h3>';
 echo 'my last name is mukesh';
 echo '</br>'; 
@@ -8,4 +13,8 @@ print_r('hello');
 echo '</br>';
 $mynewname = "Sam";
 var_dump($mynewname);
+
+$name = "sam";
+echo "<h1>My name is $name</h1>";
+echo '<h1>My name is '.$name.'</h1>';
 ?>
