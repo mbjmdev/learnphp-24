461c60fe1faae2df6765bd49fe1b5a75a946d8ba
jino_exprzn <jino@exprzn.com>
2024-08-23 11:49:16 +0530

    lesson-1

--- index.php
+++ index.php
@@ -1,20 +1,30 @@
-<?php
-// This is a single comment
-# This is a single comment
-/*
-    this is a multi line comment
-*/
-echo '<h3>Hwllo</h3>';
-echo 'my last name is mukesh';
-echo '</br>'; 
-print('hello');
-echo '</br>';
-print_r('hello');
-echo '</br>';
-$mynewname = "Sam";
-var_dump($mynewname);
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Website</title>
+    </head>
+    <body>
+        <?php
+            // This is a single comment
+            # This is a single comment
+            /*
+                this is a multi line comment
+            */
+            echo '<h3>Hwllo</h3>';
+            echo 'my last name is mukesh';
+            echo '</br>'; 
+            print('hello');
+            echo '</br>';
+            print_r('hello');
+            echo '</br>';
+            $mynewname = "Sam";
+            var_dump($mynewname);
 
-$name = "sam";
-echo "<h1>My name is $name</h1>";
-echo '<h1>My name is '.$name.'</h1>';
-?>
+            $name = "sam";
+            echo "<h1>My name is $name</h1>";
+            echo '<h1>My name is '.$name.'</h1>';
+            $active = false;
+        ?>
+        <h1 style="color: <?php if($active === true){ echo 'red'; } else { echo 'violet'; } ?>">This is my website</h1>
+    </body>
+</html>
